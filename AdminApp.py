f6b1db6d7048fe9e8af2f8b3db67c53e8483aa74
adidam23 <aditi.dam@gmail.com>
2023-04-03 14:08:32 -0400

    buttons and a table

--- AdminApp.py
+++ AdminApp.py
@@ -18,8 +18,32 @@ class Window(QWidget):
         super().__init__()
         self.setWindowTitle("Admin App")  
         self.move(0,0)
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+
+        self.generalLayout = QGridLayout()
+        # layout = QVBoxLayout()
+
+        self.addUserButton = QPushButton("Add User")
+        self.deleteUserButton = QPushButton("Delete User")
+        self.editUserButton = QPushButton("Edit User")
+
+        self.buttonGridLayout = QGridLayout()
+        self.buttonGridLayout.addWidget(self.addUserButton, 0, 0)
+        self.buttonGridLayout.addWidget(self.deleteUserButton, 0, 1)
+        self.buttonGridLayout.addWidget(self.editUserButton, 0, 2)
+        
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setHorizontalHeaderLabels(["Name", "Code"])
+
+        # Bring back later:
+        # header = self.tableWidget.horizontalHeader()
+        # header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        # header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
+
+        self.generalLayout.addLayout(self.buttonGridLayout, 0, 0)
+        self.generalLayout.addWidget(self.tableWidget, 1, 0)
+
+        self.setLayout(self.generalLayout)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
